a2609f69f4eec7e9f2b30f0b2eb0af638756b71d
Home <91093861@mvusd.net>
2022-06-07 06:18:22 -0700

    Anxiety screen

--- app/src/main/java/com/example/mindful_finalproject/Anxiety1.java
+++ app/src/main/java/com/example/mindful_finalproject/Anxiety1.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 public class Anxiety1 extends AppCompatActivity {
     private Button homeBtn;
 
-
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
